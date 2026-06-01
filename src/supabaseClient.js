19d6c93933676d4c71d9acd0758a408bc682b101
K. Rakshitha Reddy <krakshithareddy20@gmail.com>
2026-06-01 11:22:49 +0530

    CodeMentorAI

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://pmdvdunlkphexfpyxwny.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtZHZkdW5sa3BoZXhmcHl4d255Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NzQ4MDQsImV4cCI6MjA3NzE1MDgwNH0.vyyAHVy0IdpvJ9U3k4Zc-fmjuwXlIpbkV-zEQhs0UAE";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
